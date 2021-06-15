8817a532e22e21d21895e98d89c3a750355d5734
Muzaffer Övün <83140937+muzafferovun@users.noreply.github.com>
2021-06-16 00:27:15 +0300

    15-2

--- src/admin/AdminMainpage.jsx
+++ src/admin/AdminMainpage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import SessionControlService from './services/sessionControlService';
 
 export default function AdminMainpage() {
+    
+    const [userData, setUserData] = useState({});
+    useEffect(() => {
+        let sessionControlService = new SessionControlService();
+        sessionControlService.getUser().then(result => setUserData(result));
+        
+    }, [])
     return (
         <div>
-            admin
+           
         </div>
     )
 }
--- src/admin/services/sessionControlService.jsx
+++ src/admin/services/sessionControlService.jsx
@@ -1,5 +1,22 @@
+import axios from 'axios'
 import React from 'react'
 
-export default class sessionControlService(){
-  
+export default class SessionControlService{
+    getUser() {
+        let ssid=sessionStorage.getItem("ssid").toString();
+        return axios.post('http://localhost:8080/api/user/getAdminUser',
+        {
+            'sessionCode': ssid
+        },{
+            "headers": {
+              'Content-Type': 'application/json',
+            }
+        }).then((response) => {
+           console.log("reactNativeDemo","response get details:"+response.data);
+        })
+        .catch((error) => {
+           console.log("axios error:",error);
+        });
+    }
+
 }
